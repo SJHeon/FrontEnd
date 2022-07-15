432c7a2636953628dbce1b51965911695f49e7fe
SJHeon <sungjh20@gmail.com>
2022-07-15 10:39:23 +0900

    5th_commit

--- my-depl/src/setupProxy.js
+++ my-depl/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://3.87.224.78",
       changeOrigin: true,
     })
   );
