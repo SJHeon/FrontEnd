23e830f4c91eee78dd6cb4c58f6b8599bb2f78dc
SJHeon <sungjh20@gmail.com>
2022-07-15 11:58:29 +0900

    commit

--- my-depl/src/App.js
+++ my-depl/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [msg, setMsg] = useState("");
 
   useEffect(() => {
-    fetch(process.env.REACT_APP_DB_HOST + "/api/deploy")
+    fetch("/api/deploy")
       .then((response) => response.text())
       .then((msg) => {
         setMsg(msg);
